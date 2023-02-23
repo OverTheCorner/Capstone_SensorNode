b8d942fdce74fd4c4786e3e66a617b9e2dda2eb3
OverTheCorner <gabrielalmario99@yahoo.com.ph>
2023-02-23 08:43:21 +0800

    Added dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include <Arduino.h>
+#include <Adafruit_AS7341.h>
 
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
   digitalWrite(LED_BUILTIN, 0);
+  Serial.println("LED OFF");
   delay(1000);
   digitalWrite(LED_BUILTIN, 1);
+  Serial.println("LED ON");
   delay(1000);
 }
