7b068ea966f79993d4eb1b6947d56da0e8e0671d
OverTheCorner <gabrielalmario99@yahoo.com.ph>
2023-03-01 08:49:55 +0800

    Can send array data through text

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,25 @@
 #include <RTClib.h>
 #include <SoftwareSerial.h>
 
+#define NUM_OF_DATA_CHANNELS 11
+
 void gsmInit();
 void sendTextMsg();
 void updateSerial();
+void waitForKey();
+void updateChannelData();
 
 SoftwareSerial sim800l(10, 11); // RX, TX pins
+int channelData[NUM_OF_DATA_CHANNELS];
+String textMessage;
 
 void setup()
 {
   Serial.begin(9600);
+  gsmInit();
+  waitForKey();
+  updateChannelData();
+  sendTextMsg();
 }
 
 void loop()
@@ -50,14 +60,44 @@ void gsmInit()
 
 void sendTextMsg()
 {
+  textMessage = "201900044\nChannel Data:\n";
+
+  for (int i = 0; i < NUM_OF_DATA_CHANNELS; i++)
+  {
+    textMessage += String(channelData[i]);
+    textMessage += ",";
+  }
+  textMessage += "\n";
+
+  Serial.println("Will now proceed to send a text message...");
   sim800l.println("AT+CMGF=1"); // Set SMS text mode
   delay(1000);
   sim800l.println("AT+CMGS=\"+639274325235\""); // Replace with your phone number
   delay(1000);
-  sim800l.println("Hello from Arduino!"); // SMS message
+  sim800l.println(textMessage); // SMS message
   delay(1000);
   sim800l.println((char)26); // End of message character
   delay(1000);
-
   Serial.println("Message sent!");
+}
+
+void waitForKey()
+{
+  // prompt user for input
+  Serial.println("Please enter any character to continue:");
+  while (!Serial.available())
+  {
+    // wait for user to enter input
+  }
+  Serial.read(); // read and discard input
+  Serial.println("Key pressed! will now proceed with program");
+}
+
+void updateChannelData()
+{
+  for (int i = 0; i < NUM_OF_DATA_CHANNELS; i++)
+  {
+    channelData[i] = i;
+    Serial.println(channelData[i]);
+  }
 }
